6ca906ed7e0c1e1fe1f3e223bc05b7a6c3afba82
Gamezardashvili George <ggamezar@gmail.com>
2023-06-21 22:22:27 +0300

    Make env options parsing generic in EmbeddedClient

--- src/Server/EmbeddedClient/EmbeddedClient.cpp
+++ src/Server/EmbeddedClient/EmbeddedClient.cpp
@@ -12,67 +12,17 @@ namespace DB
 
 namespace ErrorCodes
 {
-    extern const int BAD_ARGUMENTS;
     extern const int LOGICAL_ERROR;
 }
 
 namespace
 {
 
-String getEnvOption(const NameToNameMap & envVars, const String & key, const String & defaultvalue)
+template<typename T>
+T getEnvOption(const NameToNameMap & envVars, const String & key, T defaultValue)
 {
     auto it = envVars.find(key);
-    return it == envVars.end() ? defaultvalue : it->second;
-}
-
-
-[[maybe_unused]] Int64 getEnvOptionInt64(const NameToNameMap & envVars, const String & key, Int64 defaultvalue)
-{
-    auto raw = getEnvOption(envVars, key, "");
-    return raw.empty() ? defaultvalue : std::stoll(raw);
-}
-
-
-UInt64 getEnvOptionUInt64(const NameToNameMap & envVars, const String & key, UInt64 defaultvalue)
-{
-    auto raw = getEnvOption(envVars, key, "");
-    return raw.empty() ? defaultvalue : std::stoull(raw);
-}
-
-
-int getEnvOptionInt(const NameToNameMap & envVars, const String & key, int defaultvalue)
-{
-    auto raw = getEnvOption(envVars, key, "");
-    return raw.empty() ? defaultvalue : std::stoi(raw);
-}
-
-
-[[maybe_unused]] unsigned int getEnvOptionUInt(const NameToNameMap & envVars, const String & key, unsigned int defaultvalue)
-{
-    auto raw = getEnvOption(envVars, key, "");
-    return raw.empty() ? defaultvalue : static_cast<unsigned int>(std::stoul(raw));
-}
-
-
-bool getEnvOptionBool(const NameToNameMap & envVars, const String & key, bool defaultvalue)
-{
-    auto raw = getEnvOption(envVars, key, "");
-    if (raw.empty())
-    {
-        return defaultvalue;
-    }
-    if (raw == "true" || raw == "1")
-    {
-        return true;
-    }
-    else if (raw == "false" || raw == "0")
-    {
-        return false;
-    }
-    else
-    {
-        throw Exception(ErrorCodes::BAD_ARGUMENTS, "Bad option provided for key {}", key);
-    }
+    return it == envVars.end() ? defaultValue : parse<T>(it->second);
 }
 
 }
@@ -143,44 +93,44 @@ try
     setThreadName("LocalServerPty");
     thread_status.emplace();
 
-    print_stack_trace = getEnvOptionBool(envVars, "stacktrace", false);
+    print_stack_trace = getEnvOption<bool>(envVars, "stacktrace", false);
 
     output_stream << std::fixed << std::setprecision(3);
     error_stream << std::fixed << std::setprecision(3);
 
     is_interactive = stdin_is_a_tty;
-    static_query = first_query.empty() ? getEnvOption(envVars, "query", "") : first_query;
+    static_query = first_query.empty() ? getEnvOption<String>(envVars, "query", "") : first_query;
     delayed_interactive = is_interactive && !static_query.empty();
     if (!is_interactive || delayed_interactive)
     {
-        echo_queries = getEnvOptionBool(envVars, "echo", false) || getEnvOptionBool(envVars, "verbose", false);
-        ignore_error = getEnvOptionBool(envVars, "ignore_error", false);
+        echo_queries = getEnvOption<bool>(envVars, "echo", false) || getEnvOption<bool>(envVars, "verbose", false);
+        ignore_error = getEnvOption<bool>(envVars, "ignore_error", false);
         is_multiquery = true;
     }
-    load_suggestions = (is_interactive || delayed_interactive) && !getEnvOptionBool(envVars, "disable_suggestion", false);
+    load_suggestions = (is_interactive || delayed_interactive) && !getEnvOption<bool>(envVars, "disable_suggestion", false);
     if (load_suggestions)
     {
-        suggestion_limit = getEnvOptionInt(envVars, "suggestion_limit", 10000);
+        suggestion_limit = getEnvOption<Int32>(envVars, "suggestion_limit", 10000);
     }
 
 
-    enable_highlight = getEnvOptionBool(envVars, "highlight", true);
-    multiline = getEnvOptionBool(envVars, "multiline", false);
+    enable_highlight = getEnvOption<bool>(envVars, "highlight", true);
+    multiline = getEnvOption<bool>(envVars, "multiline", false);
 
-    default_database = getEnvOption(envVars, "database", "");
+    default_database = getEnvOption<String>(envVars, "database", "");
 
-    format = getEnvOption(envVars, "output-format", getEnvOption(envVars, "format", is_interactive ? "PrettyCompact" : "TSV"));
+    format = getEnvOption<String>(envVars, "output-format", getEnvOption<String>(envVars, "format", is_interactive ? "PrettyCompact" : "TSV"));
     insert_format = "Values";
-    insert_format_max_block_size = getEnvOptionUInt64(envVars, "insert_format_max_block_size",
+    insert_format_max_block_size = getEnvOption<size_t>(envVars, "insert_format_max_block_size",
         global_context->getSettingsRef().max_insert_block_size);
 
 
-    server_display_name = getEnvOption(envVars, "display_name", getFQDNOrHostName());
-    prompt_by_server_display_name = getEnvOption(envVars, "prompt_by_server_display_name", "{display_name} :) ");
+    server_display_name = getEnvOption<String>(envVars, "display_name", getFQDNOrHostName());
+    prompt_by_server_display_name = getEnvOption<String>(envVars, "prompt_by_server_display_name", "{display_name} :) ");
     std::map<String, String> prompt_substitutions{{"display_name", server_display_name}};
     for (const auto & [key, value] : prompt_substitutions)
         boost::replace_all(prompt_by_server_display_name, "{" + key + "}", value);
-    initTtyBuffer(toProgressOption(getEnvOption(envVars, "progress", "default")));
+    initTtyBuffer(toProgressOption(getEnvOption<String>(envVars, "progress", "default")));
 
     if (is_interactive)
     {

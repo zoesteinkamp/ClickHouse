02fd58e1098ac8b830fef2cb62709ee68463518b
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-10-04 10:29:16 +0200

    fix TablesStatusResponse

--- src/Server/TCPHandler.cpp
+++ src/Server/TCPHandler.cpp
@@ -1279,6 +1279,9 @@ void TCPHandler::processTablesStatusRequest()
     }
 
 
+    if (out->isCanceled())
+        return;
+
     writeVarUInt(Protocol::Server::TablesStatusResponse, *out);
 
     /// For testing hedged requests
